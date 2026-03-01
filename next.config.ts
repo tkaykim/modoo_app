3a54855720264870507a923620f047d6f9908681
Daniel <sungeun8877@gmail.com>
2026-03-01 16:26:07 +0900

    fix: change cors policy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/convert-image',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://admin.modoogoods.com',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'POST, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
