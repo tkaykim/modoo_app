8933d978236c45090722097316a18e35ea834782
Eun Won Lee <sungeun8877@gmail.com>
2026-03-18 23:00:18 +0900

    feat: many fixes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -18,20 +18,20 @@ export default function Header({
 
   return (
     <header className="bg-white/70 backdrop-blur-md shadow-sm lg:shadow-none sticky top-0 z-50 border-b border-white/40">
-        <div className="max-w-7xl mx-auto px-3 sm:px-0">
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center justify-between h-16 gap-4">
+        <div className="max-w-7xl mx-auto px-2 sm:px-3 lg:px-0">
+          <div className="flex flex-col">
+            <div className="flex items-center justify-between h-11 sm:h-14 lg:h-16 gap-2 sm:gap-4">
               {/* If back is enabled show the back button instead */}
               <div className="flex items-center lg:w-48">
                 {
                   back ? (
                     backHref ? (
                       <Link href={backHref}>
-                        <ArrowLeft className="text-gray-700 size-6"/>
+                        <ArrowLeft className="text-gray-700 size-5 sm:size-6"/>
                       </Link>
                     ) : (
                       <button className="" onClick={() => router.back()}>
-                        <ArrowLeft className="text-gray-700 size-6"/>
+                        <ArrowLeft className="text-gray-700 size-5 sm:size-6"/>
                       </button>
                     )
                   ) :
@@ -41,7 +41,7 @@ export default function Header({
                         className="text-lg font-black tracking-[0.18em] text-gray-900"
                         aria-label="MODOO 홈"
                       >
-                        <img src="/icons/modoo_logo.png" alt="모두의 유니폼"  className="w-[80px]"/>
+                        <img src="/icons/modoo_logo.png" alt="모두의 유니폼"  className="w-15 sm:w-20"/>
                       </Link>
                     ) : (
                       <button className="size-6">
@@ -58,7 +58,7 @@ export default function Header({
                   className="text-lg font-black tracking-[0.18em] text-gray-900"
                   aria-label="MODOO 홈"
                 >
-                  <img src="/icons/modoo_logo.png" alt="모두의 유니폼"  className="w-[80px]"/>
+                  <img src="/icons/modoo_logo.png" alt="모두의 유니폼"  className="w-15 sm:w-20"/>
                 </Link>
               )}
 
