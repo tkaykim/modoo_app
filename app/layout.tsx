14c6b4be47c04ae4e27df8da536dde58eb797466
tkaykim <tommy0621@naver.com>
2026-04-27 01:20:13 +0900

    chore(analytics): remove direct gtag.js load, unify on GTM container
    Eliminates duplicate page_view firing (gtag.js direct + GTM both
    targeted G-5PCV010T7K). GTM container now sole loader of GA4.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import ChatBubble from "./components/chatbot/ChatBubble";
 import ChatWindow from "./components/chatbot/ChatWindow";
 import { getSiteUrl } from "@/lib/site-url";
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export const metadata: Metadata = {
@@ -45,20 +44,6 @@ export default function RootLayout({
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
             }}
           />
-        )}
-        {GA_ID && (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-            />
-            <script
-              id="ga-init"
-              dangerouslySetInnerHTML={{
-                __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${GA_ID}');`,
-              }}
-            />
-          </>
         )}
       </head>
       <body className="antialiased">
